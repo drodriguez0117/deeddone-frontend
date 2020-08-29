7aaff5647ba2272e567b9cc2c6cc3a2d36b16240
drodriguez0117 <drodriguez.0117@gmail.com>
2020-08-29 13:55:53 -0400

    scope signin form
    crete the signin module with methods for signin
    some paths will need to be refactored
    create axios instances and hook these up
    in main.js. Not sure we need the secured and
    plain instances

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,5 +1,72 @@
 <template>
-  <div>
-    <h2> Add Signin Component</h2>
-  </div>
+  <form class="form-signin" @submit.prevent="signin">
+    <div class="alert alert-danger" v-if="error">{{ error }}</div>
+    <div class="form-group">
+      <label for="email">Email Address</label>
+      <input v-model="email" type="email" class="form-control" id="email" placeholder="email@domain.com">
+    </div>
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input v-model="password" type="password" class="form-control" id="password" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-primary mb-3">Sign In</button>
+    <div>
+      <router-link to="/signup">Sign Up</router-link>
+    </div>
+  </form>
 </template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  created () {
+    this.checkSignedIn()
+  },
+  updated () {
+    this.checkSignedIn()
+  },
+  methods: {
+    signin () {
+      this.$http.plain.post('/signin', { email: this.email, password: this.password })
+        .then(response => this.signinSuccessful(response))
+        .catch(error => this.signinFailed(error))
+    },
+    signinSuccessful (response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response)
+        return
+      }
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.error = ''
+      this.$router.replace('/signin')
+    },
+    signinFailed (error) {
+      this.error = (error.response && error.response.data && error.response.data.error)
+      delete localStorage.csrf
+      delete localStorage.signedIn
+    },
+    checkSignedIn () {
+      if (localStorage.signedIn) {
+        this.$router.replace('/signin')
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css">
+.form-signin {
+  width: 70%;
+  max-width: 500px;
+  padding: 10% 15px;
+  margin: 0 auto;
+}
+</style>
