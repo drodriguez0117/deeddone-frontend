1c3b3ee85f440baf62b9937039a5e071bb34de6a
drodriguez0117 <drodriguez.0117@gmail.com>
2021-03-14 09:54:43 -0400

    refactor to remove vuetify

--- src/components/listings/Listings.vue
+++ src/components/listings/Listings.vue
@@ -1,33 +1,18 @@
 <template>
   <div class="listings">
     <h3> {{ this.$store.getters['users/getCurrentUserName'] }} Listings</h3>
-    <v-text-field v-model="search_term"></v-text-field>
-    <v-btn v-on:click="search">Search</v-btn>
-    <form action="http://localhost:8080/api/v1/admin/listings"
-      enctype="multipart/form-data"
-      method="post">
-      <input type="file" accept="image/*" ref="inputImage" @change=uploadImage() multiple>
-      <v-btn v-on:click="login" v-show="!this.$store.getters['users/getCurrentUserName']">Login</v-btn>
-      <v-btn v-on:click="logout" v-show="this.$store.getters['users/getCurrentUserName']">Logout</v-btn>
-    </form>
-    <h3> {{ status }} </h3>
-    <v-alert
-      v-if="error"
-      text
-      prominent
-      type="error"
-      icon="mdi-cloud-alert"
-    >
-        {{ error }}
-    </v-alert>
-    <v-btn v-on:click="clickMe" v-show="this.$store.getters['users/getCurrentUserName']">Create New Card</v-btn>
-    <v-container>
-      <v-row dense>
-        <v-col cols="3" v-for="listing in visibleListings" v-bind:key="listing.id">
+    <input v-on:click="login" v-show="!this.$store.getters['users/getCurrentUserName']" value="Login">
+    <input v-on:click="logout" v-show="this.$store.getters['users/getCurrentUserName']" value="Logout">
+    <input type="text" size="50" class="showBorder" v-model="search_term">
+    <input type="button" v-on:click="search" value="Search">
+    <input v-on:click="createListing" v-show="this.$store.getters['users/getCurrentUserName']" value="Create Listing">
+    <div>
+      <div class="grid">
+        <div v-for="listing in activeListings" v-bind:key="listing.id" >
           <listing-card v-bind:listing="listing"></listing-card>
-        </v-col>
-      </v-row>
-    </v-container>
+        </div>
+      </div>
+    </div>
     <br />
   </div>
 </template>
@@ -47,9 +32,7 @@ export default {
   data () {
     return {
       filteredUserId: null,
-      error: '',
       search_term: '',
-      status: 'This should be a status from create',
       images: [],
       form: new FormData()
     }
@@ -59,7 +42,7 @@ export default {
     ...mapActions('listings', ['fetchListings', 'createListing', 'searchListings']),
     ...mapActions('users', ['signOut']),
 
-    clickMe () {
+    createListing () {
       this.$router.push('/listings/create')
     },
     login () {
@@ -68,64 +51,37 @@ export default {
     logout () {
       this.signOut()
     },
-    uploadImage () {
-      // this works for a single file
-      // let imag = this.$refs.inputImage.files[0]
-      // this.form.append('listing[images][]', imag)
-
-      var fileCount = this.$refs.inputImage.files.length
-
-      for (var i = 0; i < fileCount; i++) {
-        this.form.append('listing[images][]', this.$refs.inputImage.files[i])
-      }
-    },
-    setError (error, text) {
-      this.error = (error.response && error.response.data && error.response.data.error)
-    },
     getListings () {
       this.fetchListings()
     },
-    // logout () {
-    //   console.log('logout')
-    //   var es = this.signOut(this.$store.state.users.currentUser)
-
-    //   if (es) {
-    //     this.filteredUserId = null
-    //   }
-    // },
     setUserId () {
       this.filteredUserId = this.getUserId()
-    },
-    createCard () {
-      const cardProperties = {
-        title: 'test title',
-        description: 'only come out at night',
-        listing_type: 'offering',
-        category_id: 1
-      }
-
-      Object.entries(cardProperties).forEach(
-        ([key, value]) => this.form.append('listing[' + key + ']', value)
-      )
-
-      this.createListing(this.form)
-        .then((response) => {
-          this.status = 'card created!'
-          this.form = new FormData()
-          this.fetchListings()
-        })
-        .catch((error) => {
-          this.setError(error)
-        })
     },
     search () {
       this.searchListings(this.search_term)
     }
   },
   computed: {
-    visibleListings: function () {
+    activeListings: function () {
       return this.$store.getters['listings/getFilteredListings'](this.filteredUserId)
     }
   }
 }
 </script>
+
+<style scoped>
+h3 {
+  margin: 20px;
+}
+.showBorder {
+  border: 2px solid red;
+  border-radius: 4px;
+}
+.grid {
+  height: 100%;
+  margin: 30px 100px;
+  padding: 10px 100px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+</style>
