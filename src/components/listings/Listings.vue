a60a3c85f4c7fc4223afb1aa0be185419e8695c0
drodriguez0117 <drodriguez.0117@gmail.com>
2021-07-17 16:01:09 -0400

    cleanup listings
    remove unused code
    refactor method names

--- src/components/listings/Listings.vue
+++ src/components/listings/Listings.vue
@@ -6,11 +6,9 @@
     <form action="http://localhost:8080/api/v1/admin/listings"
       enctype="multipart/form-data"
       method="post">
-      <input type="file" accept="image/*" ref="inputImage" @change=uploadImage() multiple>
       <v-btn v-on:click="login" v-show="!this.$store.getters['users/getCurrentUserName']">Login</v-btn>
       <v-btn v-on:click="logout" v-show="this.$store.getters['users/getCurrentUserName']">Logout</v-btn>
     </form>
-    <h3> {{ status }} </h3>
     <v-alert
       v-if="error"
       text
@@ -20,10 +18,10 @@
     >
         {{ error }}
     </v-alert>
-    <v-btn v-on:click="clickMe" v-show="this.$store.getters['users/getCurrentUserName']">Create New Card</v-btn>
+    <v-btn v-on:click="createListing" v-show="this.$store.getters['users/getCurrentUserName']">Create New Card</v-btn>
     <v-container>
       <v-row dense>
-        <v-col cols="3" v-for="listing in visibleListings" v-bind:key="listing.id">
+        <v-col cols="3" v-for="listing in activeListings" v-bind:key="listing.id">
           <listing-card v-bind:listing="listing"></listing-card>
         </v-col>
       </v-row>
@@ -49,7 +47,6 @@ export default {
       filteredUserId: null,
       error: '',
       search_term: '',
-      status: 'This should be a status from create',
       images: [],
       form: new FormData()
     }
@@ -59,7 +56,7 @@ export default {
     ...mapActions('listings', ['fetchListings', 'createListing', 'searchListings']),
     ...mapActions('users', ['signOut']),
 
-    clickMe () {
+    createListing () {
       this.$router.push('/listings/create')
     },
     login () {
@@ -68,62 +65,21 @@ export default {
     logout () {
       this.signOut()
     },
-    uploadImage () {
-      // this works for a single file
-      // let imag = this.$refs.inputImage.files[0]
-      // this.form.append('listing[images][]', imag)
-
-      var fileCount = this.$refs.inputImage.files.length
-
-      for (var i = 0; i < fileCount; i++) {
-        this.form.append('listing[images][]', this.$refs.inputImage.files[i])
-      }
-    },
     setError (error, text) {
       this.error = (error.response && error.response.data && error.response.data.error)
     },
     getListings () {
       this.fetchListings()
     },
-    // logout () {
-    //   console.log('logout')
-    //   var es = this.signOut(this.$store.state.users.currentUser)
-
-    //   if (es) {
-    //     this.filteredUserId = null
-    //   }
-    // },
     setUserId () {
       this.filteredUserId = this.getUserId()
-    },
-    createCard () {
-      const cardProperties = {
-        title: 'test title',
-        description: 'only come out at night',
-        listing_type: 'offering',
-        category_id: 1
-      }
-
-      Object.entries(cardProperties).forEach(
-        ([key, value]) => this.form.append('listing[' + key + ']', value)
-      )
-
-      this.createListing(this.form)
-        .then((response) => {
-          this.status = 'card created!'
-          this.form = new FormData()
-          this.fetchListings()
-        })
-        .catch((error) => {
-          this.setError(error)
-        })
     },
     search () {
       this.searchListings(this.search_term)
     }
   },
   computed: {
-    visibleListings: function () {
+    activeListings: function () {
       return this.$store.getters['listings/getFilteredListings'](this.filteredUserId)
     }
   }
